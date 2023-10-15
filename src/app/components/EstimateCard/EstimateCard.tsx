65f88252cffa0f5542c5863d91be5b5c2cd629e4
Filip Krajcar <fkrajcar@proton.me>
2023-10-15 21:56:19 +0200

    fix: minor color adjusts

--- src/app/components/EstimateCard/EstimateCard.tsx
+++ src/app/components/EstimateCard/EstimateCard.tsx
@@ -9,9 +9,9 @@ export const EstimateCard = ({ value, displayData }: IEstimateCardProps) => {
   return (
     <Paper radius="md" withBorder className={classes.card} mt={20}>
       <Badge
-        size="xl"
         className={classes.icon}
-        style={{ fontSize: 24, minWidth: rem(92), height: rem(92) }}
+        style={{ minWidth: rem(92), height: rem(92) }}
+        color="blue.5"
       >
         {displayData && value.toFixed(1)}
       </Badge>
@@ -29,7 +29,11 @@ export const EstimateCard = ({ value, displayData }: IEstimateCardProps) => {
         </Text>
       </Group>
 
-      <Progress value={displayData ? (value / 24) * 100 : 0} mt={5} />
+      <Progress
+        color="blue.5"
+        value={displayData ? (value / 24) * 100 : 0}
+        mt={5}
+      />
     </Paper>
   )
 }

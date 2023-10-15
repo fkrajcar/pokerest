c51d0b591ba4900315a28bb7abd3971277184b20
Filip Krajcar <fkrajcar@proton.me>
2023-10-15 03:07:45 +0200

    feature: miscs

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -22,7 +22,7 @@ import {
   setDoc,
   updateDoc,
 } from 'firebase/firestore'
-import { ChangeEvent, useEffect, useState } from 'react'
+import { ChangeEvent, useCallback, useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { useDisclosure } from '@mantine/hooks'
 import { Button, Loader, Modal, TextInput } from '@mantine/core'
@@ -35,8 +35,6 @@ interface IUser {
 }
 
 export default function Room({ params }: IRoomPageParams) {
-  const [estimatedValue, setEstimatedValue] = useState<number | null>(null)
-  const [estimateId, setEstimateId] = useState<string | null>(null)
   const [isLoading, setIsLoading] = useState<boolean>(true)
   const [currentUser, setCurrentUser] = useState<IUser | null>(null)
   const [users, setUsers] = useState<IUser[]>([])
@@ -49,7 +47,7 @@ export default function Room({ params }: IRoomPageParams) {
   const handleChange = (event: ChangeEvent<HTMLInputElement>) =>
     setUsername(event?.target?.value)
 
-  const getNotes = async () => {
+  const init = async () => {
     const docRef = doc(database, 'rooms', params.id)
 
     const docSnap = await getDoc(docRef)
@@ -63,7 +61,6 @@ export default function Room({ params }: IRoomPageParams) {
     }
 
     userChangeSnapshotListener()
-    estimateChangeSnapshotListener()
     displayDataChangeListener()
   }
 
@@ -122,17 +119,32 @@ export default function Room({ params }: IRoomPageParams) {
       setUsers(newUsers)
     })
   }
+  const joinRoom = useCallback(
+    async (cookieUser: IUser) => {
+      const response = await setDoc(
+        doc(database, 'rooms', params.id, 'users', cookieUser.id),
+        {
+          username: cookieUser.username,
+        },
+        { merge: true }
+      )
 
-  const estimateChangeSnapshotListener = () => {
-    let collectionRef = collection(dbInstance, params.id, 'estimates')
+      console.log('joinRoom', cookieUser.id, { response })
+    },
+    [params?.id]
+  )
 
-    onSnapshot(collectionRef, (querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        const docData = doc.data()
-        console.log('Estimates data: ', docData, { users })
-      })
-    })
-  }
+  const removeFromRoom = useCallback(
+    async (cookieUser: IUser | null) => {
+      if (!cookieUser?.id) {
+        console.error('nema userId, removeFromRoom')
+        return
+      }
+
+      await deleteDoc(doc(database, 'rooms', params.id, 'users', cookieUser.id))
+    },
+    [params?.id]
+  )
 
   useEffect(() => {
     setIsLoading(true)
@@ -153,34 +165,13 @@ export default function Room({ params }: IRoomPageParams) {
         // getData(cookieUser.id)
       }
     }
-    getNotes()
+    init()
     setIsLoading(false)
 
     return () => {
       removeFromRoom(currentUser)
     }
   }, [])
-
-  const joinRoom = async (cookieUser: IUser) => {
-    const response = await setDoc(
-      doc(database, 'rooms', params.id, 'users', cookieUser.id),
-      {
-        username: cookieUser.username,
-      },
-      { merge: true }
-    )
-
-    console.log('joinRoom', cookieUser.id, { response })
-  }
-
-  const removeFromRoom = async (cookieUser: IUser | null) => {
-    if (!cookieUser?.id) {
-      console.error('nema userId, removeFromRoom')
-      return
-    }
-
-    await deleteDoc(doc(database, 'rooms', params.id, 'users', cookieUser.id))
-  }
 
   const saveUser = async () => {
     let collectionRef = collection(dbInstance, params.id, 'users')
@@ -238,43 +229,14 @@ export default function Room({ params }: IRoomPageParams) {
     }
   }
 
-  const getValues = async () => {
-    console.log('test')
-    let collectionRef = collection(dbInstance, params.id, 'estimates')
-    // const docSnap = await getDoc(collectionRef)
-    const querySnapshot = await getDocs(collectionRef)
-    console.log({ querySnapshot })
-    const data = querySnapshot.docs
-    console.log({ data })
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, ' => ', doc.data())
-    })
-  }
-
-  const displayEstimates = async () => {
+  const setDisplayEstimates = async (display: boolean) => {
     const roomsRef = doc(dbInstance, params.id)
 
-    const res = await updateDoc(roomsRef, {
-      displayEstimates: true,
+    await updateDoc(roomsRef, {
+      displayEstimates: display,
     })
 
-    console.log('displayEstimates', { res })
-    // const cityRef = dbInstance.collection('cities').doc('DC');
-
-    // Set the 'capital' field of the city
-  }
-  const hideEstimates = async () => {
-    const roomsRef = doc(dbInstance, params.id)
-
-    const res = await updateDoc(roomsRef, {
-      displayEstimates: false,
-    })
-
-    console.log('displayEstimates', { res })
-    // const cityRef = dbInstance.collection('cities').doc('DC');
-
-    // Set the 'capital' field of the city
+    getAverage()
   }
 
   const resetEstimates = async () => {
@@ -293,6 +255,23 @@ export default function Room({ params }: IRoomPageParams) {
         )
       })
     )
+
+    await setDisplayEstimates(false)
+  }
+
+  const getAverage = () => {
+    const estimates = users
+      .map((user) => user.estimate)
+      .filter((estimate) => !!estimate)
+    console.log(estimates)
+    var sum = estimates.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue
+    }, 0)
+
+    const avg = sum / estimates?.length
+    console.log(avg)
+
+    return avg
   }
 
   return (
@@ -312,19 +291,17 @@ export default function Room({ params }: IRoomPageParams) {
               </p>
             ))}
       </div>
-      {/* {!!estimatedValue && <span>{estimatedValue}</span>} */}
-      {currentUser?.estimate && <span>{currentUser?.estimate}</span>}
 
-      <Button onClick={() => getValues()}>Get values</Button>
       <Button onClick={() => removeFromRoom(currentUser)}>
         Remove from room
       </Button>
-      <Button onClick={displayEstimates}>Display estimates</Button>
-      <Button onClick={hideEstimates}>Hide estimates</Button>
+      <Button onClick={() => setDisplayEstimates(!displayData)}>
+        {displayData ? 'Hide estimates' : 'Display estimates'}
+      </Button>
       <Button color="red" onClick={resetEstimates}>
         Reset estimates
       </Button>
-      {displayData && <div>Pokazi svima</div>}
+      {displayData && <div>Average: {getAverage()}</div>}
       <div>
         {[2, 4, 6, 8].map((estimateValue) => (
           <Button
